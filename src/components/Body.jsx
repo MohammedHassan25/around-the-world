1b100d67efb01dc20a024e2d34320a50a0a7dcaa
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2025-03-14 15:18:33 +0200

    Some modifications to transfer fetchData to father components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,64 @@
+import { useEffect, useState } from "react";
 import { Countries, SearchAndRegion } from "./index";
 
 export function Body() {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  async function fetchData() {
+    setLoading(true);
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json();
+      const filteredData = data.filter(
+        (country) => country.name?.common !== "Israel",
+      );
+      const country = filteredData.map((country) => {
+        return {
+          name: country.name?.common,
+          nativeName: country.name?.nativeName
+            ? Object.values(country.name.nativeName)[0].common
+            : null,
+          population: country.population,
+          currencies: country.currencies
+            ? Object.values(country.currencies)[0]?.name
+            : null,
+          region: country.region,
+          subregion: country.subregion
+            ? country.subregion
+            : country.name?.common,
+          capital: country.capital ? country.capital[0] : null,
+          flag: country.flags?.png,
+          domain: country.tld ? country.tld[0] : null,
+          languages: country.languages
+            ? Object.values(country.languages)[0]
+            : null,
+        };
+      });
+      return country;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const data = await fetchData();
+      setCountries(data);
+    };
+    fetchCountries();
+  }, []);
   return (
     <div className="bg-gray-100 px-0 pb-16 pt-12 dark:bg-gray-900 md:px-20">
-      <SearchAndRegion />
-      <Countries />
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <>
+          <SearchAndRegion />
+          <Countries countries={countries} />
+        </>
+      )}
     </div>
   );
 }
