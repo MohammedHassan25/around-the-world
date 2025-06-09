f7c4de0fc93e98b6e12db40c32d2a226fe683057
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2025-06-09 19:27:46 +0300

    add scroll up

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -7,6 +7,7 @@ export function MainPage() {
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [scrollY, setScrollY] = useState(0);
 
   async function fetchData() {
     setLoading(true);
@@ -45,7 +46,9 @@ export function MainPage() {
       localStorage.setItem("countries", JSON.stringify(country));
       return country;
     } catch (error) {
-      const storedCountries = localStorage.getItem("countries")  ? localStorage.getItem("countries") : JSON.stringify(DataCountries);
+      const storedCountries = localStorage.getItem("countries")
+        ? localStorage.getItem("countries")
+        : JSON.stringify(DataCountries);
       return storedCountries ? JSON.parse(storedCountries) : null;
     } finally {
       setLoading(false);
@@ -64,6 +67,19 @@ export function MainPage() {
     fetchCountries();
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScrollY(scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="font-inter dark:text-gray-100">
       <Header />
@@ -77,6 +93,28 @@ export function MainPage() {
           fetchData,
         }}
       />
+      {scrollY >= 200 && (
+        <div className="fixed bottom-4 right-4 rounded-full bg-blue-500 p-2 text-white shadow-lg hover:bg-blue-600 cursor-pointer" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+          <a
+            className="flex h-full w-full items-center justify-center transition-all duration-300 ease-in-out"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-arrow-up"
+            >
+              <line x1="12" y1="19" x2="12" y2="5" />
+              <polyline points="5 12 12 5 19 12" />
+            </svg>
+          </a>
+        </div>
+      )}
     </div>
   );
 }
